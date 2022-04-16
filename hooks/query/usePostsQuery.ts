c01038ab386a82b2c61b4f61607dc96f25eb4893
sujin-park <psujin831@gmail.com>
2022-04-16 11:56:48 +0900

    feat: react-query 설정, 가이드라인 코드 추가

---
+++ hooks/query/usePostsQuery.ts
@@ -0,0 +1,22 @@
+import Error from 'next/error';
+import { useQuery } from 'react-query';
+import { apiClient } from '@shared/api/apiClient';
+import QueryKey from '@shared/constants/queryKey';
+
+// TODO: 예시 interface -> 이후 제거
+interface Post {
+  id: number;
+  title: string;
+}
+
+const fetchPosts = async (): Promise<Post[]> => {
+  const { data } = await apiClient.get('/posts');
+
+  return data;
+};
+
+const usePostsQuery = () => {
+  return useQuery<Post[], Error>(QueryKey.GET_POSTS, fetchPosts);
+};
+
+export { usePostsQuery, fetchPosts };
