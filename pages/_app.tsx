a36293f63b65d8682d4ef840fdbcac5ff5606ced
sujin-park <psujin831@gmail.com>
2022-04-16 11:56:48 +0900

    feat: react-query 설정, 가이드라인 코드 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,26 @@
-import '@/styles/globals.css';
-import React from 'react';
+import '@styles/globals.css';
+import React, { useState } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import theme from '@/styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </QueryClientProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { usePostsQuery } from '@hooks/query';
 import Example from '../components/Example/Example';
 
 const Home = () => {
+  const posts = usePostsQuery();
+
+  if (posts.isLoading) {
+    return <p>Loading</p>;
+  }
+
   return <Example />;
 };
+
+if (process.env.NODE_ENV === 'development') {
+  if (typeof window === 'undefined') {
+    (async () => {
+      const { server } = await import('@mocks/server');
+      server.listen();
+    })();
+  } else {
+    (async () => {
+      const { worker } = await import('@mocks/browser');
+      worker.start();
+    })();
+  }
+}
 
 export default Home;
