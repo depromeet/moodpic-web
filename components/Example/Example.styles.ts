49d6e24172180fecfbb11d5f4127913b25578c0a
sangbooom <tkdqja4164@naver.com>
2022-04-19 01:04:24 +0900

    feat: 앱 레이아웃(컨테이너) 잡기

--- components/Example/Example.styles.ts
+++ components/Example/Example.styles.ts
@@ -2,8 +2,6 @@ import styled from 'styled-components';
 import theme from '@/styles/theme';
 
 export const ExampleWrap = styled.div`
-  background-color: ${theme.colors.primary}; // 방법 1
-  color: ${({ theme }) => theme.colors.black}; // 방법 2
-  width: 400px;
-  height: 400px;
+  /* background-color: ${theme.colors.primary}; // 방법 1
+  color: ${({ theme }) => theme.colors.black}; // 방법 2 */
 `;
