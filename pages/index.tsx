e70093d22a74c70755b0346d40a4c21979a2cb32
sujin-park <psujin831@gmail.com>
2022-04-23 23:38:21 +0900

    feat: main page 에 Banner, Tab 컴포넌트 추가 - feat: main page 에 Banner, Tab import - feat: Tab 관련 상수 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import { usePostsQuery } from '@/hooks/query';
-import Example from '../components/Example/Example';
+import React, { useState } from 'react';
+import HomeBanner from '@/components/Home/Banner';
+import HomeTab, { CurrentTabType } from '@/components/Home/Tab';
+import { HOME_TAB_TYPE } from '@/shared/constants/home';
 
 const Home = () => {
-  const posts = usePostsQuery();
+  const [currentTab, setCurrentTab] = useState<CurrentTabType>(
+    HOME_TAB_TYPE.FOLDER,
+  );
 
-  if (posts.isLoading) {
-    return <p>Loading</p>;
-  }
-
-  return <Example />;
+  return (
+    <>
+      <HomeBanner nickname="홍길동" />
+      <HomeTab
+        currentTab={currentTab}
+        setCurrentTab={(tab: CurrentTabType) => setCurrentTab(tab)}
+        onClick={() => console.log('폴더 추가')}
+      />
+    </>
+  );
 };
 
 if (process.env.NODE_ENV === 'development') {
