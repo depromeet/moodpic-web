d0749b9d097432fb12dfd2d31604776ae6d27d11
sujin-park <psujin831@gmail.com>
2022-04-23 21:07:43 +0900

    feat: Chip button 컴포넌트 추가

---
+++ components/Common/ChipButton/ChipButton.tsx
@@ -0,0 +1,32 @@
+import React, { MouseEventHandler } from 'react';
+import CloseIcon from '@/assets/icons/close.svg';
+import { ChipButtonContainer, Text, CloseImage } from './ChipButton.styles';
+
+export interface ChipButtonProps {
+  canDelete?: boolean;
+  onDelete: MouseEventHandler<HTMLImageElement>;
+  children: React.ReactNode;
+}
+
+const ChipButton = ({
+  canDelete = false,
+  onDelete,
+  children,
+}: ChipButtonProps): React.ReactElement => {
+  return (
+    <ChipButtonContainer>
+      <Text canDelete={canDelete}>{children}</Text>
+      {canDelete && (
+        <CloseImage
+          src={CloseIcon}
+          alt="삭제"
+          width={16}
+          height={16}
+          onClick={onDelete}
+        />
+      )}
+    </ChipButtonContainer>
+  );
+};
+
+export default ChipButton;
