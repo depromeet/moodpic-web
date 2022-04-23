a7981ad092de66365f74a0ccd35f12752b358e3b
sujin-park <psujin831@gmail.com>
2022-04-24 01:08:13 +0900

    feat: TabHeader, Tabs 컴포넌트 분리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import HomeBanner from '@/components/Home/Banner';
-import HomeTab, { CurrentTabType } from '@/components/Home/Tab';
+import HomeTabHeader from '@/components/Home/TabHeader';
+import HomeTabs, { CurrentTabType } from '@/components/Home/Tabs';
 import { HOME_TAB_TYPE } from '@/shared/constants/home';
+import styled from 'styled-components';
 
 const Home = () => {
   const [currentTab, setCurrentTab] = useState<CurrentTabType>(
@@ -11,14 +13,20 @@ const Home = () => {
   return (
     <>
       <HomeBanner nickname="홍길동" />
-      <HomeTab
+      <HomeTabHeader currentTab={currentTab} />
+      <HomeTabs
         currentTab={currentTab}
         setCurrentTab={(tab: CurrentTabType) => setCurrentTab(tab)}
         onClick={() => console.log('폴더 추가')}
       />
+      <Contents />
     </>
   );
 };
+
+const Contents = styled.div`
+  height: 1500px;
+`;
 
 if (process.env.NODE_ENV === 'development') {
   if (typeof window === 'undefined') {
