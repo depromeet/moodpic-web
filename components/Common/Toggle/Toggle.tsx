747c1326bbe928af47142011b8585aeb1c43c6ed
sujin-park <psujin831@gmail.com>
2022-04-23 13:02:59 +0900

    feat: Toggle button 추가

---
+++ components/Common/Toggle/Toggle.tsx
@@ -0,0 +1,27 @@
+import React, { ChangeEventHandler } from 'react';
+import {
+  ToggleWrapper,
+  Trigger,
+  Icon,
+  IconWrapper,
+  ToggleText,
+} from './Toggle.styles';
+
+export interface ToggleProps {
+  checked: boolean;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+const Toggle = ({ checked, onChange }: ToggleProps): React.ReactElement => {
+  return (
+    <ToggleWrapper>
+      <Trigger type="checkbox" checked={checked} onChange={onChange} />
+      <IconWrapper>
+        <Icon />
+        <ToggleText checked={checked}>{checked ? 'on' : 'off'}</ToggleText>
+      </IconWrapper>
+    </ToggleWrapper>
+  );
+};
+
+export default Toggle;
