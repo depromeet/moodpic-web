fedc215242be6b9aecd362d7a0730555fc095444
sangbooom <tkdqja4164@naver.com>
2022-04-24 18:38:29 +0900

    feat: 코드 수정

--- components/Example/Example.tsx
+++ components/Example/Example.tsx
@@ -2,9 +2,9 @@ import React, { useCallback, useState } from 'react';
 import Image from 'next/image';
 import BottomSheetContainer from '@/components/Common/BottomSheetContainer/BottomSheetContainer';
 import BottomSheetFolerList from '@/components/Common/BottomSheetFolderList/BottomSheetFolerList';
+import BottomSheetShare from '@/components/Common/BottomSheetShare/BottomSheetShare';
 
 import FolderIcon from 'public/svgs/folder.svg';
-import BottomSheetShare from '../Common/BottomSheetShare/BottomSheetShare';
 
 const mockResponse = [
   {
@@ -31,6 +31,18 @@ const mockResponse = [
     folderId: 6,
     folderName: '폴더명6',
   },
+  {
+    folderId: 7,
+    folderName: '폴더명7',
+  },
+  {
+    folderId: 8,
+    folderName: '폴더명8',
+  },
+  {
+    folderId: 9,
+    folderName: '폴더명9',
+  },
 ];
 
 const Example = () => {
@@ -56,9 +68,15 @@ const Example = () => {
         <BottomSheetContainer
           onClose={onClickModal}
           BottomSheetHeight={calcBottomSheetHeight()}
+          headerTitle={
+            <>
+              <Image src={FolderIcon} alt="folderIcon" />
+              <div>변경할 폴더를 선택해주세요.</div>
+            </>
+          }
         >
-          {/* <BottomSheetFolerList folderData={mockResponse} /> */}
-          <BottomSheetShare />
+          <BottomSheetFolerList folderData={mockResponse} />
+          {/* <BottomSheetShare /> */}
         </BottomSheetContainer>
       ) : null}
     </>
