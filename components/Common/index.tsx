29321d39484aac5b24f304df6483ee59a27c6fa3
sangbooom <tkdqja4164@naver.com>
2022-04-24 23:22:02 +0900

    feat: dialog

--- components/Common/index.tsx
+++ components/Common/index.tsx
@@ -1,2 +1,3 @@
 export { default as Header } from './Header/Header';
 export { default as BottomSheetContainer } from './BottomSheetContainer/BottomSheetContainer';
+export { default as Dialog } from './Dialog/Dialog';
