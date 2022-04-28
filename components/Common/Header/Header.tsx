f5ed377086e6daaeaa823020252b542dc3df38d8
sujin-park <psujin831@gmail.com>
2022-04-28 22:19:54 +0900

    test: Main Header 컴포넌트 storybook 추가

--- components/Common/Header/Header.tsx
+++ components/Common/Header/Header.tsx
@@ -3,17 +3,20 @@ import Image from 'next/image';
 import { HeaderWrapper, Title, TitleWrapper } from './Header.styles';
 import MagnifyingGlassIcon from 'public/svgs/magnifyingglass.svg';
 import LeftIcon from 'public/svgs/left.svg';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
+
   // TODO: main header 이후 다른 페이지 헤더 작업 예정
   const MainHeader = () => (
     <>
       <TitleWrapper>
         <Title>서비스명</Title>
-        <Image src={LeftIcon} alt="메뉴" />
+        <Image src={LeftIcon} alt="메뉴" width={16} height={16} />
       </TitleWrapper>
-      <button>
-        <Image src={MagnifyingGlassIcon} alt="검색" />
+      <button onClick={() => router.push('/search')}>
+        <Image src={MagnifyingGlassIcon} alt="검색" width={24} height={24} />
       </button>
     </>
   );
