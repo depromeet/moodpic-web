90775cf7e06b7b0f6d63f1bf7a60cb8dfc85adec
sujin-park <psujin831@gmail.com>
2022-04-28 22:07:20 +0900

    feat: main header 컴포넌트 추가

--- components/Common/Header/Header.tsx
+++ components/Common/Header/Header.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { HeaderWrapper } from './Header.styles';
+import Image from 'next/image';
+import { HeaderWrapper, Title, TitleWrapper } from './Header.styles';
+import MagnifyingGlassIcon from 'public/svgs/magnifyingglass.svg';
+import LeftIcon from 'public/svgs/left.svg';
 
-const Header = () => (
-  <>
-    <HeaderWrapper>header</HeaderWrapper>
-  </>
-);
+const Header = () => {
+  // main header 먼저 작업
+  const MainHeader = () => (
+    <>
+      <TitleWrapper>
+        <Title>서비스명</Title>
+        <Image src={LeftIcon} alt="메뉴" />
+      </TitleWrapper>
+      <button>
+        <Image src={MagnifyingGlassIcon} alt="검색" />
+      </button>
+    </>
+  );
+
+  return (
+    <>
+      <HeaderWrapper>
+        <MainHeader />
+      </HeaderWrapper>
+    </>
+  );
+};
 
 export default Header;
