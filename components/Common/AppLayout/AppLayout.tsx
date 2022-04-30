5053fce0ea503ab1f9963c520e23e2f846aff30b
sujin-park <psujin831@gmail.com>
2022-04-30 12:03:26 +0900

    feat: dropdownState atom 추가, dropdown portal destination 추가

--- components/Common/AppLayout/AppLayout.tsx
+++ components/Common/AppLayout/AppLayout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
 import { Header } from '@/components/Common';
 import { Toast } from '@/components/Common';
 import { Container, ContainerInner } from './AppLayout.styles';
 import { toastStateAtom } from '@/store/toast/atom';
-import { useRecoilValue } from 'recoil';
+import DropdownMenu from '../DropdownMenu/DropdownMenu';
+import { dropdownStateAtom } from '@/store/dropdown/atom';
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -11,10 +13,13 @@ interface AppLayoutProps {
 
 const AppLayout = ({ children }: AppLayoutProps): React.ReactElement => {
   const toastType = useRecoilValue(toastStateAtom);
+  const dropdownState = useRecoilValue(dropdownStateAtom);
+
   return (
     <>
       <Container>
         <ContainerInner>
+          {dropdownState && <DropdownMenu />}
           <Header />
           {children}
         </ContainerInner>
