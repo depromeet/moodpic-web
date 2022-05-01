ab502d8930f136d475bf0f45862a22f3951d8890
sujin-park <psujin831@gmail.com>
2022-05-01 11:44:56 +0900

    feat: editMode 에 따라서 편집,삭제 버튼 노출

--- components/Home/Folder/Folder.styles.ts
+++ components/Home/Folder/Folder.styles.ts
@@ -2,16 +2,13 @@ import styled from 'styled-components';
 import theme from '@/styles/theme';
 
 export const FolderContainer = styled.figure`
+  position: relative;
   width: 100%;
   height: 100%;
 `;
 
-export const Caption = styled.figcaption`
-  padding-top: 8px;
-`;
-
 export const FolderName = styled.p`
-  margin-bottom: 4px;
+  margin-bottom: 0.4rem;
   ${theme.fonts.btn2};
   color: ${theme.colors.white};
 `;
@@ -21,10 +18,24 @@ export const FolderCount = styled.span`
   color: ${theme.colors.gray4};
 `;
 
-export const ImageConatiner = styled.div`
-  height: 100%;
-`;
-
 export const BoxContainer = styled.figure`
   display: flex;
+`;
+
+export const CaptionContainer = styled.figcaption`
+  display: flex;
+  align-items: flex-start;
+  margin-top: 0.8rem;
+`;
+
+export const DeleteButton = styled.button`
+  position: absolute;
+  top: 0.8rem;
+  right: 0.8rem;
+`;
+
+export const EditButton = styled.button`
+  width: 2.4rem;
+  height: 2.4rem;
+  margin-right: 0.8rem;
 `;
