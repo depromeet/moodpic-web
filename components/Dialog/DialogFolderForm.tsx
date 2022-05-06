c9676b9f1821ee6d53a2121d58a3f3bbe4609e05
sujin-park <psujin831@gmail.com>
2022-05-06 16:24:58 +0900

    feat: Dialog content component 추가

---
+++ components/Dialog/DialogFolderForm.tsx
@@ -0,0 +1,45 @@
+import React, { ChangeEventHandler } from 'react';
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+import { CommonTextField } from '../Common';
+
+interface DialogFolderFormProps {
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+const DialogFolderForm = ({ value, onChange }: DialogFolderFormProps) => {
+  return (
+    <DialogContainer>
+      <Title>📁 새폴더의 이름을 입력해주세요.</Title>
+      <CommonTextField
+        placeholder="폴더명 입력"
+        borderRadius="0.4rem"
+        supportsMaxLength
+        maxLength={10}
+        value={value}
+        hasBorder={true}
+        onChange={onChange}
+      />
+    </DialogContainer>
+  );
+};
+
+const DialogContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2.4rem 1.6rem 1.6rem;
+
+  > div {
+    width: 100%;
+  }
+`;
+
+const Title = styled.p`
+  ${theme.fonts.h4};
+  color: ${theme.colors.white};
+  margin-bottom: 2rem;
+`;
+
+export default DialogFolderForm;
