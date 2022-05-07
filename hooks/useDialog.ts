de0f9d4d50c7959baa31832e2224ff7592cdcaa7
sujin-park <psujin831@gmail.com>
2022-05-07 10:42:31 +0900

    fix: 잘못 작성한 라인 제거

--- hooks/useDialog.ts
+++ hooks/useDialog.ts
@@ -6,7 +6,6 @@ export default function useModal(initialMode = false) {
   const toggleDialog = (): void => {
     setDialogVisible(!dialogVisible);
     document.body.style.overflow = dialogVisible ? 'hidden' : 'unset';
-    document.body.style.overflow = 'unset';
   };
 
   return { dialogVisible, toggleDialog };
