6e41987f6aeb5ef5a6345eb4d84b1726b7a03a51
sujin-park <psujin831@gmail.com>
2022-05-08 13:26:43 +0900

    feat: AppBar 컴포넌트 undefined-feelings 페이지에 추가

--- pages/posts/undefined-feelings.tsx
+++ pages/posts/undefined-feelings.tsx
@@ -1,28 +1,47 @@
 import React from 'react';
-import PostItem from '@/components/Post/PostItem/PostItem';
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import theme from '@/styles/theme';
 import { Post } from '@/shared/type/post';
+import PostItem from '@/components/Post/PostItem/PostItem';
+import { CommonAppBar, CommonIconButton } from '@/components/Common';
 
 const postList: Post[] = [];
 
 const PostList = () => {
+  const router = useRouter();
+
   return (
-    <PostListContainer>
-      <Title>
-        몰라요를 선택한 기록들, <br />
-        지금은 어떤 감정이 드세요?
-      </Title>
-      {postList.length ? (
-        postList.map((post) => (
-          <li key={post.id}>
-            <PostItem post={post} isMine={false} />
-          </li>
-        ))
-      ) : (
-        <GuideMessage>기록이 없어요.</GuideMessage>
-      )}
-    </PostListContainer>
+    <>
+      <CommonAppBar>
+        <CommonAppBar.Left>
+          <CommonIconButton
+            iconName="left"
+            alt="이전"
+            onClick={() => router.back()}
+          />
+        </CommonAppBar.Left>
+      </CommonAppBar>
+      <PostListContainer>
+        <Title>
+          몰라요를 선택한 기록들, <br />
+          지금은 어떤 감정이 드세요?
+        </Title>
+        {postList.length ? (
+          postList.map((post) => (
+            <li key={post.id}>
+              <PostItem
+                post={post}
+                isMine={false}
+                onClick={() => router.push(`/posts/${post.id}`)}
+              />
+            </li>
+          ))
+        ) : (
+          <GuideMessage>기록이 없어요.</GuideMessage>
+        )}
+      </PostListContainer>
+    </>
   );
 };
 
