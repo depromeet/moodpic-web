d5856e7cd82532b201fd27b2288cac6e92ce7472
sujin-park <psujin831@gmail.com>
2022-05-12 08:43:56 +0900

    feat: 상세 페이지 content 부분 Question style 재사용해서 추가

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -9,6 +9,13 @@ import {
 import Card from '@/components/Card/Card';
 import theme from '@/styles/theme';
 import { EMOTION_COLOR_TYPE } from '@/shared/constants/emotion';
+import { CONTENT_SEPARATOR } from '@/shared/constants/question';
+import {
+  NumberTitle,
+  ProvidedQuestionMainTitle,
+  ProvidedQuestionWrap,
+} from '@/components/Question/Question.styles';
+import { CommonTextArea } from '@/components/Common';
 
 const PostDetail = () => {
   const router = useRouter();
@@ -19,10 +26,14 @@ const PostDetail = () => {
     tags: ['우울해', '대박피곤해', '이거모야', '이거모야', '이거모야'],
     firstCategory: 'SADNESS',
     secondCategory: 'DONTKNOW',
-    content: '어쩌고 저쩌고',
+    content:
+      '카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나|안녕하세요|안녕하세요',
     hit: 3333,
     createdAt: '2021년 02월 04일 16:45',
   };
+
+  const hasMultipleContent = post.content.includes(CONTENT_SEPARATOR);
+  const contents = post.content.split(CONTENT_SEPARATOR);
 
   return (
     <>
@@ -41,12 +52,50 @@ const PostDetail = () => {
             <CommonTagButton key={index}>#{tag}</CommonTagButton>
           ))}
         </TagContainer>
-        <Card
-          firstColor={EMOTION_COLOR_TYPE[post.firstCategory]}
-          secondColor={EMOTION_COLOR_TYPE[post.secondCategory]}
-        >
-          그땐 {post.firstCategory}, 지금은 {post.secondCategory}
-        </Card>
+        <CardContainer>
+          <Card
+            firstColor={EMOTION_COLOR_TYPE[post.firstCategory]}
+            secondColor={EMOTION_COLOR_TYPE[post.secondCategory]}
+          >
+            그땐 {post.firstCategory}, 지금은 {post.secondCategory}
+          </Card>
+        </CardContainer>
+        {hasMultipleContent ? (
+          <QuestionContainer>
+            <ProvidedQuestionWrap>
+              <NumberTitle>
+                <span>1</span>
+                /3
+              </NumberTitle>
+              <MultipleLineText>
+                카톡이름님에게 <br /> 어떤 일이 있었나요?
+              </MultipleLineText>
+              <CommonTextArea value={contents[0]} height="32.6rem" readOnly />
+            </ProvidedQuestionWrap>
+            <ProvidedQuestionWrap>
+              <NumberTitle>
+                <span>2</span>
+                /3
+              </NumberTitle>
+              <ProvidedQuestionMainTitle>
+                그 때 어떤 감정이 들었나요?
+              </ProvidedQuestionMainTitle>
+              <CommonTextArea value={contents[1]} height="32.6rem" readOnly />
+            </ProvidedQuestionWrap>
+            <ProvidedQuestionWrap>
+              <NumberTitle>
+                <span>3</span>
+                /3
+              </NumberTitle>
+              <ProvidedQuestionMainTitle>
+                고생했어요! 스스로에게 한마디를 쓴다면?
+              </ProvidedQuestionMainTitle>
+              <CommonTextArea value={contents[2]} height="32.6rem" readOnly />
+            </ProvidedQuestionWrap>
+          </QuestionContainer>
+        ) : (
+          <CommonTextArea value={post.content} height="32.6rem" readOnly />
+        )}
         <Description>조회수 {post.hit}</Description>
         <Description>{post.createdAt}</Description>
       </PostDetailContainer>
@@ -84,7 +133,6 @@ const Description = styled.p`
   color: ${theme.colors.gray4};
   margin-left: auto;
 
-  // TODO: Textfield 추가하고 해당 selector 수정
   div ~ & {
     margin-top: 1.4rem;
   }
@@ -92,6 +140,18 @@ const Description = styled.p`
   & ~ & {
     margin-top: 1rem;
   }
+`;
+
+const CardContainer = styled.div`
+  margin-bottom: 2.4rem;
+`;
+
+const MultipleLineText = styled(ProvidedQuestionMainTitle)`
+  line-height: 160%;
+`;
+
+const QuestionContainer = styled.div`
+  margin-bottom: -2.6rem;
 `;
 
 export default PostDetail;
