b429ad1f7f9b44a308059ac388dff3765205510d
g1moon <gmoon.cho@gmail.com>
2022-05-12 03:01:18 +0900

    feat(wip): axios 인스턴스 draft (의견 여쭙기용 커밋)

--- shared/api/apiClient.ts
+++ shared/api/apiClient.ts
@@ -1,11 +1,30 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import {
+  getLocalStorageValue,
+  removeLocalStorageValue,
+} from '../utils/localStorage';
+import { LOCAL_STORAGE_KEY } from '../constants/localStorageKey';
 
 // TODO: 이후 production baseURL 수정
 const baseURL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '';
 
 const axiosInstance = axios.create({
   baseURL,
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  // const authToken = getLocalStorageValue(LOCAL_STORAGE_KEY.AUTH_TOKEN);
+  // // auth관련 작업
+  // return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    removeLocalStorageValue(LOCAL_STORAGE_KEY.AUTH_TOKEN);
+    // push(ROUTES.LOGIN);
+  },
+);
 
 export const apiClient = axiosInstance;
