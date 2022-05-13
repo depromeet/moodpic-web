21f2608ef76e800f61ae491928c4d4dd9c47ee0e
sujin-park <psujin831@gmail.com>
2022-05-13 21:52:51 +0900

    refactor: msw start 부분 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,18 +35,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-if (process.env.NODE_ENV === 'development') {
-  if (typeof window === 'undefined') {
-    (async () => {
-      const { server } = await import('@/mocks/server');
-      server.listen();
-    })();
-  } else {
-    (async () => {
-      const { worker } = await import('@/mocks/browser');
-      worker.start();
-    })();
-  }
-}
-
 export default MyApp;
