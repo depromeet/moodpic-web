b2d228df24f7b33ad39ede7bfb66992a7a1514fc
Gimoon Cho <44131043+guymoon@users.noreply.github.com>
2022-05-13 23:53:38 +0900

    feat: 소셜 로그인 관련 기능 추가 (#104)
    * feat: localStorage 유틸 함수 추가
      * feat: getLocalStorageValue
      * feat: setLocalStorageValue
      * feat: removeLocalStorageValue
    
    * feat: LocalStorage 관련 상수 추가
      * feat: LocalStorageKey 타입 추가
      * feat: LOCAL_STORAGE_KEY 상수 추가
    
    * feat(wip): auth 요청 관련 service 객체 draft(의견 여쭙기용 커밋)
    
    * feat(wip): axios 인스턴스 draft (의견 여쭙기용 커밋)
    
    * feat: ROUTES 관련 상수 추가
    
    * chore: auth 관련 상수 추가
      - KAKAO_CLIENT_ID
      - KAKAO_REDIRECT_URL
    
    * feat: axios 관련 fetcher 유틸함수 추가
      * feat: paramsSerializer 추가
      * refactor: baseURL 로컬 서버에서 배포 서버로 변경
      * feat: interceptors.request.use 추가
        - 로컬스토리지에서 토큰 값 가져오고, 있으면 헤더에 추가
    
    * feat: kakao 인증 페이지 추가
    
    * feat: routes 관련 상수 추가
    
    * feat: 로그인 임시 페이지 추가
      * feat: 카카오 로그인 버튼 누르면 카카오 로그인 진행하는 기능 추가
    
    * feat: userService try-catch로 감싸기
    
    * refactor

--- shared/api/apiClient.ts
+++ shared/api/apiClient.ts
@@ -1,4 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import {
+  getLocalStorageValue,
+  removeLocalStorageValue,
+} from '../utils/localStorage';
+import { LOCAL_STORAGE_KEY } from '../constants/localStorageKey';
 
 // TODO: 이후 production baseURL 수정
 const baseURL =
@@ -7,5 +12,19 @@ const baseURL =
 const axiosInstance = axios.create({
   baseURL,
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  // const authToken = getLocalStorageValue(LOCAL_STORAGE_KEY.AUTH_TOKEN);
+  // // auth관련 작업
+  // return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    removeLocalStorageValue(LOCAL_STORAGE_KEY.AUTH_TOKEN);
+    // push(ROUTES.LOGIN);
+  },
+);
 
 export const apiClient = axiosInstance;
