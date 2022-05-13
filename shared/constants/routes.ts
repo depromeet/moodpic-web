96614c605aa0aedfee51a822cd2163bb7b464a8d
g1moon <gmoon.cho@gmail.com>
2022-05-13 23:21:36 +0900

    feat: routes 관련 상수 추가

--- shared/constants/routes.ts
+++ shared/constants/routes.ts
@@ -1,4 +1,8 @@
+import { KAKAO_CLIENT_ID, KAKAO_REDIRECT_URL } from './auth';
+
 export const ROUTES = {
-  AUTH_CALLBACK_KAKAO: '/auth/callback/kakao',
-  LOGIN: '/auth',
+  HOME: '/',
+  AUTH_CALLBACK_KAKAO: '/oauth/callback/kakao',
+  LOGIN: '/oauth',
+  KAKAO_AUTH: `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`,
 } as const;
