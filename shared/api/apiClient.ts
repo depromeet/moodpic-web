f293182a871e1d7013cdcb97027c4ceea4c3c33c
sujin-park <psujin831@gmail.com>
2022-05-13 21:52:51 +0900

    refactor: msw start 부분 제거

--- shared/api/apiClient.ts
+++ shared/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // TODO: 이후 production baseURL 수정
 const baseURL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '';
 
 const axiosInstance = axios.create({
   baseURL,
