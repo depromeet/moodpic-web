6090dd55101ec10145722bde346bb5a4552e21b4
sujin-park <psujin831@gmail.com>
2022-05-14 22:21:13 +0900

    refactor: useToast function -> notify function 에 object 전달하게 수정

--- components/Example/ToastExample.tsx
+++ components/Example/ToastExample.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import useToast from '@/hooks/useToast';
 
 const ToastExample = () => {
-  const notify = useToast({
-    type: 'error',
-    message: '이미 존재하는 폴더명이에요.',
-  });
+  const notify = useToast();
+
+  const showToast = () => {
+    notify({
+      type: 'error',
+      message: '이미 존재하는 폴더명이에요.',
+    });
+  };
 
   return (
     <div>
-      <button onClick={notify} style={{ backgroundColor: 'yellow' }}>
+      <button onClick={showToast} style={{ backgroundColor: 'yellow' }}>
         토스트 띄우기
       </button>
     </div>
