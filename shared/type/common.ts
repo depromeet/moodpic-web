5c240d78f2eadb647d38330bb2f8110f213725a7
sujin-park <psujin831@gmail.com>
2022-05-19 22:12:07 +0900

    refactor: ToastType type common 으로 이동

--- shared/type/common.ts
+++ shared/type/common.ts
@@ -13,3 +13,11 @@ export interface PageType {
   page?: number;
   size?: number;
 }
+
+export const ToastType = {
+  ERROR: 'error',
+  WARNING: 'warning',
+  CONFIRM: 'confirm',
+} as const;
+
+export type ToastType = typeof ToastType[keyof typeof ToastType];
