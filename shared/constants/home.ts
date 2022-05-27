5b860e284632579f3819f71a3c3f4727ece946a0
sujin-park <psujin831@gmail.com>
2022-05-28 00:38:25 +0900

    feat: BANNER_TITLE_CASES, TOTAL_BANNER_BACKGROUND_SIZE 추가

--- shared/constants/home.ts
+++ shared/constants/home.ts
@@ -11,3 +11,12 @@ export const HOME_TAB_LABEL = {
 } as const;
 
 export type CurrentTabType = typeof HOME_TAB_TYPE[keyof typeof HOME_TAB_TYPE];
+
+export const BANNER_TITLE_CASES = [
+  '님의 <br />모든 감정은 소중해잉',
+  '님, 감정을 되짚어보면 <br />기분이 나아질거예요!',
+  '님, 오늘 어떤 일이 <br />있었는지 들려주세요!',
+  '님, 오늘의 감정을 <br />풀어보는 시간을 가져볼까요?',
+];
+
+export const TOTAL_BANNER_BACKGROUND_SIZE = 3;
