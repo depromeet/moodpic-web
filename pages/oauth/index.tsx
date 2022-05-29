aa321a94527fd086bc01d919aa589eff378a895e
sujin-park <psujin831@gmail.com>
2022-05-29 22:09:58 +0900

    fix: kakao redirect url location -> window.location.origin 으로 변경

--- pages/oauth/index.tsx
+++ pages/oauth/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
-import { ROUTES } from '../../shared/constants/routes';
+import { KAKAO_CLIENT_ID, KAKAO_REDIRECT_URL } from '@/shared/constants/auth';
 
 const Login = () => {
   const router = useRouter();
 
   const goKakaoCallback = async () => {
-    router.push(ROUTES.KAKAO_AUTH);
+    const kakaoRedirectURL = `${window.location.origin}${KAKAO_REDIRECT_URL}`;
+    router.push(
+      `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${kakaoRedirectURL}&response_type=code`,
+    );
   };
 
   return (
