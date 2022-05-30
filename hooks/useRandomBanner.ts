fb4faccd1c8827a2d83c8b08ddaf63f207677859
sujin-park <psujin831@gmail.com>
2022-05-30 19:47:40 +0900

    refactor: dangerouslySetInnerHTML -> React element, background -> image 로 변경

--- hooks/useRandomBanner.ts
+++ hooks/useRandomBanner.ts
@@ -1,12 +1,10 @@
-import { BANNER_TITLE_CASES, TOTAL_BANNER_BACKGROUND_SIZE } from '@/shared/constants/home';
+import { TOTAL_BANNER_BACKGROUND_SIZE } from '@/shared/constants/home';
 
 export const useRandomBanner = () => {
   const randomIndex = Math.floor(TOTAL_BANNER_BACKGROUND_SIZE * Math.random()) + 1;
-  const randomTitle = BANNER_TITLE_CASES[Math.floor(BANNER_TITLE_CASES.length * Math.random())];
   const randomImageSource = `/images/main-banner${randomIndex}.png`;
 
   return {
-    randomTitle,
     randomImageSource,
   };
 };
