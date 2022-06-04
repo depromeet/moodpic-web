5cb8a487a5228a5baca32907ba5940b2f2951773
sujin-park <psujin831@gmail.com>
2022-06-04 11:08:19 +0900

    fix: postId 종속되는 query 로 변경

--- hooks/apis/folder/useFoldersQuery.ts
+++ hooks/apis/folder/useFoldersQuery.ts
@@ -9,6 +9,6 @@ const useFoldersQuery = (): UseQueryResult<FolderResponse, AxiosError<ServerResp
   useQuery(QUERY_KEY.GET_FOLDERS, folderService.getFolders);
 
 const useFolderByPostIdQuery = (postId: string): UseQueryResult<Folder, AxiosError<ServerResponse>> =>
-  useQuery(QUERY_KEY.GET_FOLDER_BY_POST_ID, () => folderService.getFolderByPostId(postId));
+  useQuery(QUERY_KEY.GET_FOLDER_BY_POST_ID, () => folderService.getFolderByPostId(postId), { enabled: !!postId });
 
 export { useFoldersQuery, useFolderByPostIdQuery };
