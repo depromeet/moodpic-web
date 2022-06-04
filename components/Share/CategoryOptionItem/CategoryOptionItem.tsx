9898e223d32726453f7b63e210bf18d25f953986
joel <joel@chequer.io>
2022-06-04 14:45:51 +0900

    feat: CategoryOptionItem

--- components/Share/CategoryOptionItem/CategoryOptionItem.tsx
+++ components/Share/CategoryOptionItem/CategoryOptionItem.tsx
@@ -1,27 +1,66 @@
 import React from 'react';
-
-const EMOTION_OPTIONS = {
-  SORRY: 'sorry',
-  THANK: 'thank',
-} as const;
-
-type EmotionOptions = typeof EMOTION_OPTIONS[keyof typeof EMOTION_OPTIONS];
-type TmpType = { [key in EmotionOptions]: { icon: string; text: string } };
-
-const TMP: TmpType = {
-  [EMOTION_OPTIONS.SORRY]: { icon: '~~', text: '미안해요' },
-  [EMOTION_OPTIONS.THANK]: { icon: '~~', text: '감사해요' },
-}; // 다른 곳으로 넘기기
+import { CATEGORY_OPTIONS_INFO } from '../../../shared/constants/share';
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
+import Image from 'next/image';
+import CheckIcon from 'public/svgs/check.svg';
 
 interface CategoryOptionItemProps {
   isSelect: boolean;
-  category: EmotionOptions;
+  category: keyof typeof CATEGORY_OPTIONS_INFO;
+  onClick: () => void;
 }
 
-const CategoryOptionItem = ({ isSelect, category }: CategoryOptionItemProps) => {
-  // const { icon, text } = TMP.category;
+const CategoryOptionItem = ({ isSelect, category, onClick }: CategoryOptionItemProps) => {
+  const { categoryIcon, text, color } = CATEGORY_OPTIONS_INFO[category];
 
-  return <div>hello</div>;
+  return (
+    <Container onClick={onClick} bgColor={color}>
+      {isSelect && (
+        <CheckIconContainer isSelect={isSelect}>
+          <Image src={CheckIcon} alt={'checkIcon'} />
+        </CheckIconContainer>
+      )}
+      <CategoryIconContainer>
+        <Image src={categoryIcon} alt={'categoryIcon'} />
+      </CategoryIconContainer>
+      <CategoryText isNotSelect={CATEGORY_OPTIONS_INFO[category] === CATEGORY_OPTIONS_INFO.NOT_SELECT}>
+        {text}
+      </CategoryText>
+    </Container>
+  );
 };
+
+const Container = styled.div<{ bgColor: string }>`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(p) => p.bgColor};
+  width: 14rem;
+  height: 14rem;
+  border-radius: 1rem;
+  flex-basis: auto;
+  min-width: auto;
+`;
+
+// TODO: 타입 재활용 하기. Pick<CategoryOptionItemProps, 'isSelect'>으로 타입이 잡히지 않는 문제
+const CheckIconContainer = styled.div<{ isSelect: boolean }>`
+  position: absolute;
+  width: 4rem;
+  height: 4rem;
+  top: 0;
+  right: 0;
+`;
+
+const CategoryIconContainer = styled.div`
+  color: ${theme.colors.white};
+`;
+const CategoryText = styled.p<{ isNotSelect: boolean }>`
+  color: ${({ isNotSelect }) => (isNotSelect ? theme.colors.white : theme.colors.black)};
+  margin-top: 0.5rem;
+  ${theme.fonts.h5}
+`;
 
 export default CategoryOptionItem;
