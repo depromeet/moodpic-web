713e82e41dfb8d69ae8323cc7c5e5c2535c3f6d8
sujin-park <psujin831@gmail.com>
2022-06-04 16:15:03 +0900

    fix: bottomSheet isVisibleSheet false 일 때 unset 하는 훅 추가

--- hooks/useBottomSheet.ts
+++ hooks/useBottomSheet.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const MAX_SHOW_LIST_ITEM = 7;
 const HEADER_HEIGHT = 88;
@@ -22,6 +22,10 @@ export default function useBottomSheet() {
   const toggleSheet = () => {
     setVisibleSheet(!isVisibleSheet);
   };
+
+  useEffect(() => {
+    document.body.style.overflow = isVisibleSheet ? 'hidden' : 'unset';
+  }, [isVisibleSheet]);
 
   return {
     calcBottomSheetHeight,
