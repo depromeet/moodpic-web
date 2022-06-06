1ad53d0337943761200b3c7b3310601026b9b4ad
sujin-park <psujin831@gmail.com>
2022-06-06 17:15:18 +0900

    feat: AppLayout 에서 storePathValues 함수 실행

--- components/Common/AppLayout/AppLayout.tsx
+++ components/Common/AppLayout/AppLayout.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
+import { storePathValues } from '@/shared/utils/storePathValues';
+import { useRouter } from 'next/router';
 import { CommonToast } from '@/components/Common';
 import { Container, ContainerInner } from './AppLayout.styles';
 import { toastStateAtom } from '@/store/toast/atom';
@@ -11,8 +13,11 @@ interface AppLayoutProps {
 }
 
 const AppLayout = ({ children }: AppLayoutProps): React.ReactElement => {
+  const router = useRouter();
   const toastType = useRecoilValue(toastStateAtom);
   const dropdownState = useRecoilValue(dropdownStateAtom);
+
+  useEffect(() => storePathValues, [router.asPath]);
 
   return (
     <>
