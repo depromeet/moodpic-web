0309bfb426b5173b07987235b7d391d96a63178d
sujin-park <psujin831@gmail.com>
2022-06-06 17:13:16 +0900

    feat: previous path 를 얻기 위한 storePathValues util 함수 추가

---
+++ shared/utils/storePathValues.ts
@@ -0,0 +1,14 @@
+const storePathValues = () => {
+  const storage = globalThis?.sessionStorage;
+  if (!storage) return;
+  const prevPath = storage.getItem('currentPath');
+  storage.setItem('prevPath', prevPath as string);
+  storage.setItem('currentPath', globalThis.location.pathname);
+};
+
+const getPrevPath = () => {
+  const storage = globalThis?.sessionStorage;
+  return storage ? storage.getItem('prevPath') : '';
+};
+
+export { storePathValues, getPrevPath };
