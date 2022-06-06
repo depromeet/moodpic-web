d8df74321e36bcbf9465583cc2b82ec438f4ceeb
joel <joel@chequer.io>
2022-06-06 12:17:28 +0900

    feat: getMe(내 정보 가져오기)

---
+++ service/apis/memberService.ts
@@ -0,0 +1,12 @@
+import fetcher from '../../shared/utils/fetcher';
+import { Me } from '../../shared/type/member';
+
+const memberService = {
+  getMe: async (): Promise<Me> => {
+    const { data } = await fetcher('get', '/users/me');
+
+    return data;
+  },
+};
+
+export default memberService;
