402ccf4ba00d64e21e98e6c9008e8dab2faca77f
joel <joel@chequer.io>
2022-06-06 18:23:19 +0900

    refactor: useSearchedPostsQuery 하나로 통일
      - 기존 useSearchedPostsByNewest, useSearchedPostsByPopularity 로 달리하는 방식에서 변경

--- hooks/apis/search/useSearch.ts
+++ hooks/apis/search/useSearch.ts
@@ -2,18 +2,28 @@ import { useQuery, UseQueryResult } from 'react-query';
 import { AxiosError } from 'axios';
 import { ServerResponse } from 'http';
 import { QUERY_KEY } from '../../../shared/constants/queryKey';
-import { SearchedPost, TagFrequencies } from '@/shared/type/post';
+import { Post, TagFrequencies } from '@/shared/type/post';
 import searchService from '../../../service/apis/searchService';
+import { TAG_SEARCH_ORDER_TYPE, TagSearchOrderType } from '../../../shared/constants/tagSearch';
 
-const useSearchedPostsByNewest = (searchedTag: string): UseQueryResult<SearchedPost[], AxiosError<ServerResponse>> =>
-  useQuery(QUERY_KEY.GET_SEARCHED_BY_TAG, () => searchService.getSearchedPostsByNewest(searchedTag));
+const useSearchedPostsQuery = ({
+  searchedTag,
+  orderType,
+}: {
+  searchedTag: string;
+  orderType: TagSearchOrderType;
+}): UseQueryResult<Post[], AxiosError<ServerResponse>> => {
+  const getSearchedPosts =
+    orderType === TAG_SEARCH_ORDER_TYPE.NEWEST
+      ? searchService.getSearchedPostsByNewest
+      : searchService.getSearchedPostsByPopularity;
 
-const useSearchedPostsByPopularity = (
-  searchedTag: string,
-): UseQueryResult<SearchedPost[], AxiosError<ServerResponse>> =>
-  useQuery(QUERY_KEY.GET_SEARCHED_BY_TAG, () => searchService.getSearchedPostsByPopularity(searchedTag));
+  return useQuery(QUERY_KEY.GET_SEARCHED_BY_TAG, () => getSearchedPosts(searchedTag), {
+    enabled: false,
+  });
+};
 
 const usePopularTags = (): UseQueryResult<TagFrequencies[], AxiosError<ServerResponse>> =>
   useQuery(QUERY_KEY.GET_POPULAR_TAGS, () => searchService.getPopularTags());
 
-export { useSearchedPostsByNewest, useSearchedPostsByPopularity, usePopularTags };
+export { useSearchedPostsQuery, usePopularTags };
