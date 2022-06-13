b189bfaacd8334c933ac1cbb788d2e5582540d48
joel <joel@chequer.io>
2022-06-13 09:48:45 +0900

    fix: safari에서 navigator.clipboard 지원하지 않아 클립보드 카피 방법 수정

--- shared/utils/copyClipboard.ts
+++ shared/utils/copyClipboard.ts
@@ -8,9 +8,20 @@ export const copyClipboard = async ({
   onFail?: () => void;
 }) => {
   try {
-    await navigator.clipboard.writeText(text);
+    const inputHasClipboardText = <HTMLInputElement>document.createElement('input');
+    inputHasClipboardText.id = 'clipboardText';
+
+    document.body.appendChild(inputHasClipboardText);
+    if (!inputHasClipboardText) return alert(404);
+    inputHasClipboardText.value = text;
+    inputHasClipboardText.setSelectionRange(0, text.length);
+    inputHasClipboardText.select();
+    document.execCommand('copy');
+    document.body.removeChild(inputHasClipboardText);
+
     onSuccess?.();
   } catch (error) {
+    console.log(error);
     onFail?.();
   }
 };
