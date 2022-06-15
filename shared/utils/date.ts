907608265b72530427dbddc9b473555d9b09d611
sujin-park <psujin831@gmail.com>
2022-06-16 00:16:11 +0900

    fix: date format on safari 버그 수정

--- shared/utils/date.ts
+++ shared/utils/date.ts
@@ -1,4 +1,10 @@
-export const formatDatetime = (date: string) => {
+export const formatDatetime = (inputDate: string) => {
+  const [date, time] = inputDate.split(' ');
+  const [year, month, day] = date.split('-');
+  const [hour, min] = time.split(':');
+
+  const dateUTC = Date.UTC(Number(year), Number(month) - 1, Number(day), Number(hour), Number(min));
+
   const formatter = new Intl.DateTimeFormat('ko-KR', {
     year: 'numeric',
     month: 'long',
@@ -6,7 +12,7 @@ export const formatDatetime = (date: string) => {
     hour: '2-digit',
     minute: '2-digit',
     hourCycle: 'h24',
-  }).formatToParts(new Date(`${date}Z`));
+  }).formatToParts(new Date(dateUTC));
 
   const formattedDate = formatter.map(({ type, value }) => (type === 'dayPeriod' ? '' : value)).join('');
   return formattedDate.replace('  ', ' ');
