a4f668dc12be5ba443595c40b616736fb3346662
sujin-park <psujin831@gmail.com>
2022-06-22 21:56:10 +0900

    feat: login 페이지 스타일 수정

--- shared/constants/auth.ts
+++ shared/constants/auth.ts
@@ -1,3 +1,4 @@
 export const KAKAO_CLIENT_ID = '6632ea97bbe44cd0282ac3afff861f67';
 export const KAKAO_REDIRECT_URL = '/oauth/callback/kakao';
 export const AUTH_TOKEN = 'authToken';
+export const KAKAO_CLIENT_ORIGIN = 'https://kauth.kakao.com/oauth/authorize';
