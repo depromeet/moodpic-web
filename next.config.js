685075a894e384ec93f56acdc7a1b5ae8e3b06a4
sangbooom <tkdqja4164@naver.com>
2022-06-24 00:22:36 +0900

    refactor: next/image config setting 및 프리로드

--- next.config.js
+++ next.config.js
@@ -18,7 +18,10 @@ module.exports = {
   },
   images: {
     domains: ['firebasestorage.googleapis.com', 'k.kakaocdn.net', '5gzoo.s3.ap-northeast-2.amazonaws.com'],
+    deviceSizes: [360, 414, 480],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
     minimumCacheTTL: 31536000,
+    formats: ['image/webp'],
   },
   webpack(config) {
     return config;
