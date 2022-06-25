3fe06b1dde421751725c3a544cc7e81d4924ccf2
sangbooom <tkdqja4164@naver.com>
2022-06-25 11:16:05 +0900

    refactor

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { BASE_URL } from '@/shared/constants/url';
+import { ROUTES } from '@/shared/constants/routes';
 
 export function middleware(req: NextRequest) {
-  if (!req.url.includes(`${BASE_URL}/oauth`) && !req.url.includes('/share/post') && !req.cookies.authToken) {
-    return NextResponse.redirect(`${BASE_URL}/oauth`);
+  if (!req.url.includes(`${BASE_URL}${ROUTES.LOGIN}`) && !req.url.includes('/share/post') && !req.cookies.authToken) {
+    return NextResponse.redirect(`${BASE_URL}${ROUTES.LOGIN}`);
   }
 }
