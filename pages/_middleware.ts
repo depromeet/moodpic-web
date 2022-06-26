16390af3d1a8d1252724f604e60d40bb4cb4d639
sangbooom <tkdqja4164@naver.com>
2022-06-26 12:16:32 +0900

    refactor

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const [, , baseUrl] = req.url.split('/');
+  const baseUrl = new URL(req.url).origin;
   if (!req.url.includes(`${baseUrl}/oauth`) && !req.url.includes('/share/post') && !req.cookies.authToken) {
     console.log('미들웨어 발생!');
     return NextResponse.redirect(`${baseUrl}/oauth`);
