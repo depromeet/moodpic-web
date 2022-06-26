279e5fef33ffa71e206c0ef8a184c54f78e8b2b3
sangbooom <tkdqja4164@naver.com>
2022-06-27 01:53:46 +0900

    hotfix: ios safari pwa 다운로드 충족 조건 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,9 +42,15 @@ class MyDocument extends Document {
             content="당신의 해소하고 싶은 감정들을 기록하고 공유해보세요. 마음이 한결 편안해질거예요!"
           />
           <meta name="theme-color" content="#121212" />
-          {/* TODO: 임시 아이콘 */}
-          <link rel="apple-touch-icon" href="/favicon.png" />
-          {/* TODO: 임시 아이콘 */}
+          {/* 주소창 등의 웹 브라우저 UI를 표시하지 않기  */}
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          {/* 상태 바의 스타일을 지정  */}
+          <meta name="apple-mobile-web-app-status-bar-style" content="#121212" />
+          {/* 홈 화면에서 표시되는 앱 이름을 지정  */}
+          <meta name="apple-mobile-web-app-title" content="moodpic" />
+          {/* 홈 화면에서 표시되는 앱 아이콘을 지정  */}
+          <link rel="apple-touch-icon" href="/icon-192x192.png" />
+          {/* TODO: 임시 아이콘 변경 */}
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
           <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
         </Head>
