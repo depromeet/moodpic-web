975d36bf227dc2cfde1eedd7abeec5154cc79b6d
sangbooom <tkdqja4164@naver.com>
2022-06-27 00:58:48 +0900

    fix: NextResponse가 있는 미들웨어는 이미지 렌더링을 차단하는 버그 우회

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,7 +4,31 @@ import { ROUTES } from '@/shared/constants/routes';
 
 export function middleware(req: NextRequest) {
   const baseUrl = new URL(req.url).origin;
-  if (!req.url.includes(`${baseUrl}${ROUTES.LOGIN}`) && !req.url.includes('/share/post') && !req.cookies.authToken) {
+
+  // reference: https://stackoverflow.com/questions/72206974/next-js-middleware-with-nextresponse-blocks-images-from-rendering
+  const unprotectedPaths: string[] = [
+    `${baseUrl}/login`,
+    `${baseUrl}/_next/webpack-hmr`,
+    `${baseUrl}/images/ANXIOUS_160x160.png`,
+    `${baseUrl}/images/RELIEF_160x160.png`,
+    `${baseUrl}/images/EASYGOING_160x160.png`,
+    `${baseUrl}/images/PROUD_160x160.png`,
+    `${baseUrl}/images/DISAPPOINTMENT_160x160.png`,
+    `${baseUrl}/images/LETHARGY_160x160.png`,
+    `${baseUrl}/images/SADNESS_160x160.png`,
+    `${baseUrl}/images/REGRET_160x160.png`,
+    `${baseUrl}/images/JOY_160x160.png`,
+    `${baseUrl}/images/CALMDOWN_160x160.png`,
+    `${baseUrl}/images/IRRITATION_160x160.png`,
+  ];
+
+  if (unprotectedPaths.includes(req.url)) {
+    return void 0;
+  } else if (
+    !req.url.includes(`${baseUrl}${ROUTES.LOGIN}`) &&
+    !req.url.includes('/share/post') &&
+    !req.cookies.authToken
+  ) {
     return NextResponse.redirect(`${baseUrl}${ROUTES.LOGIN}`);
   }
 }
