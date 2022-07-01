2de718cd2e78851d8fe5cc347ae4e83bab5ae048
joel <joel@chequer.io>
2022-07-02 01:17:05 +0900

    fix: dynamic meta tag test(name -> property)

--- components/MetaHead/MetaHead.tsx
+++ components/MetaHead/MetaHead.tsx
@@ -10,21 +10,18 @@ const MetaHead = ({ title, description }: InferGetServerSidePropsType<typeof get
     <Head>
       <meta charSet="utf-8" />
       <meta
-        name="viewport"
+        property="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
       />
       <title>
         {title}
         {description}
       </title>
-      {/*<link rel="icon" href={post.icon} />*/}
-      <meta name="description" content={description} />
-      <meta name="theme-color" content="#000000" />
-      <meta name="og:type" content="article" />
-      <meta name="og:title" content={'og:title'} />
-      <meta name="og:url" content={'og:url'} />
-      <meta name="og:description" content={'og:description'} />
-      <meta name="og:image" content={SaGong.src} />
+      <meta property="og:type" content="article" />
+      <meta key="title" property="og:title" content={'og:title'} />
+      <meta property="og:url" content={'og:url'} />
+      <meta key="description" property="og:description" content={'og:description'} />
+      <meta property="og:image" content={SaGong.src} />
     </Head>
   );
 };
