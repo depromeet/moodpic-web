0853f746de2feaaaf58c91bb0faa2afd5cf46253
sujin-park <psujin831@gmail.com>
2022-08-20 14:45:49 +0900

    fix: add container to textarea component

--- components/Common/TextArea/TextArea.styles.ts
+++ components/Common/TextArea/TextArea.styles.ts
@@ -1,31 +1,35 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { TextAreaProps } from '@/components/Common/TextArea/TextArea';
 import theme from '@/styles/theme';
 
-export const Textarea = styled.textarea<Pick<TextAreaProps, 'height' | 'borderRadius'>>`
-  width: 100%;
+export const TextareaContainer = styled.div<{ borderRadius: string; isFocused: boolean }>`
   padding: 1.6rem 1.8rem 1.8rem 1.8rem;
-  display: inline-block;
-  text-align: start;
-  cursor: text;
-  height: ${({ height }) => height};
   border-radius: ${({ borderRadius }) => borderRadius};
   background: ${theme.colors.gray2};
-  border: none;
-  right: 0;
+  box-sizing: border-box;
+
+  ${({ isFocused }) =>
+    isFocused &&
+    css`
+      border: 0.1rem solid ${theme.colors.gray4};
+    `}
+`;
+
+export const Textarea = styled.textarea<Pick<TextAreaProps, 'height'>>`
+  width: 100%;
+  height: ${({ height }) => height};
   ${theme.fonts.body}
   color: ${theme.colors.white};
+  background: inherit;
+  border: 0;
   outline: none;
   resize: none;
 
-  :focus {
-    border: 0.1rem solid ${theme.colors.gray4};
-  }
   ::placeholder {
     color: ${theme.colors.gray4};
   }
+
   :disabled {
-    color: ${theme.colors.white};
     -webkit-text-fill-color: ${theme.colors.white};
     opacity: 1;
   }
