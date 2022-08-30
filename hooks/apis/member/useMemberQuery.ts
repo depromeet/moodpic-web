ed65a81e2ab731b2e075906d5bfa5b94d42662fc
sujin-park <psujin831@gmail.com>
2022-08-30 23:34:48 +0900

    fix: useMemberQuery response type 변경

--- hooks/apis/member/useMemberQuery.ts
+++ hooks/apis/member/useMemberQuery.ts
@@ -1,9 +1,10 @@
 import { useQuery, UseQueryResult } from 'react-query';
-import { AxiosError, AxiosResponse } from 'axios';
+import { AxiosError } from 'axios';
 import { ServerResponse } from '@/shared/type/common';
 import { QUERY_KEY } from '@/shared/constants/queryKey';
 import memberService from '@/service/apis/memberService';
+import { Me } from '@/shared/type/member';
 
-export const useMemberQuery = (): UseQueryResult<AxiosResponse, AxiosError<ServerResponse>> => {
+export const useMemberQuery = (): UseQueryResult<Me, AxiosError<ServerResponse>> => {
   return useQuery(QUERY_KEY.GET_ME, () => memberService.getMe());
 };
