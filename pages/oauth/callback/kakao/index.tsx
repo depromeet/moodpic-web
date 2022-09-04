74aff931ee22cb2cd6200fa258a50daa760c1ae0
sujin-park <psujin831@gmail.com>
2022-09-04 10:57:42 +0900

    WIP

--- pages/oauth/callback/kakao/index.tsx
+++ pages/oauth/callback/kakao/index.tsx
@@ -16,21 +16,21 @@ const KakaoAuth = () => {
     }
   };
 
-  // Listen for authorization success.
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  document.addEventListener('AppleIDSignInOnSuccess', (event: any) => {
-    // Handle successful response.
-    alert(event.detail.data);
-  });
-
-  // // Listen for authorization failures.
-  // /* eslint-disable @typescript-eslint/no-explicit-any */
-  document.addEventListener('AppleIDSignInOnFailure', (event: any) => {
-    // Handle error.
-    alert(event.detail.error);
-  });
-
   useEffect(() => {
+    // Listen for authorization success.
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    document.addEventListener('AppleIDSignInOnSuccess', (event: any) => {
+      // Handle successful response.
+      alert(event.detail.data);
+    });
+
+    // // Listen for authorization failures.
+    // /* eslint-disable @typescript-eslint/no-explicit-any */
+    document.addEventListener('AppleIDSignInOnFailure', (event: any) => {
+      // Handle error.
+      alert(event.detail.error);
+    });
+
     const kakaoCode = new URL(window.location.href).searchParams.get('code');
     if (kakaoCode) {
       login(kakaoCode);
