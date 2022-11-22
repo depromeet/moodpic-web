01aab0c9da18d4862bcdd84b7c44972479349ea9
박수진A <psujin831@gmail.com>
2022-11-22 23:15:44 +0900

    Revert "refactor: 제거되는 Home Banner 관련 상수, 컴포넌트 제거"
    This reverts commit a93fa8060dfce922b416e9cb558802454d7294a1.

---
+++ components/Home/Banner/Banner.tsx
@@ -0,0 +1,79 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+import { CommonButton } from '@/components/Common';
+import theme from '@/styles/theme';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export interface BannerProps {
+  nickname: string;
+  title: ReactNode;
+  background: string;
+}
+
+const Banner = ({ nickname, title, background }: BannerProps): React.ReactElement => (
+  <BannerContainer>
+    <BannerImage />
+    <Image src={background} alt="" layout="fill" priority={true} />
+    <BannerContents>
+      <BannerTitle>
+        {nickname}
+        {title}
+      </BannerTitle>
+      <Link href="/write">
+        <CommonButtonWrapper>
+          <CommonButton>감정 기록하기 ✍🏻</CommonButton>
+        </CommonButtonWrapper>
+      </Link>
+    </BannerContents>
+  </BannerContainer>
+);
+
+const CommonButtonWrapper = styled.a`
+  display: block;
+`;
+
+const BannerContainer = styled.section`
+  position: relative;
+  height: 24.8rem;
+  margin-right: -1.8rem;
+  margin-left: -1.8rem;
+  padding: 4.4rem 1.8rem 2.4rem;
+`;
+
+const BannerImage = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 33.6rem;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(180deg, #121212 42.71%, rgba(18, 18, 18, 0) 100%);
+    z-index: 1;
+  }
+`;
+
+const BannerContents = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 33.6rem;
+  padding: 13.2rem 1.8rem 2.4rem;
+  z-index: 2;
+`;
+
+const BannerTitle = styled.div`
+  ${theme.fonts.subtitle1};
+  color: ${theme.colors.white};
+  margin-bottom: 5.4rem;
+  white-space: pre-wrap;
+`;
+
+export default Banner;
--- shared/constants/home.ts
+++ shared/constants/home.ts
@@ -12,4 +12,12 @@ export const HOME_TAB_LABEL = {
 
 export type CurrentTabType = typeof HOME_TAB_TYPE[keyof typeof HOME_TAB_TYPE];
 
+export const BANNER_TITLE_CASES = [
+  '님의 <br />모든 감정은 소중해잉 🥺',
+  '님, 감정을 되짚어보면 <br />기분이 나아질거예요!',
+  '님, 오늘 어떤 일이 <br />있었는지 들려주세요!',
+  '님, 오늘의 감정을 <br />풀어보는 시간을 가져볼까요?',
+];
+
+export const TOTAL_BANNER_BACKGROUND_SIZE = 3;
 export const MINIMUM_FOLDER_SIZE = 1;
